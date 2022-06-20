f8a2c24b86f2ae0ec7b476f58055b936fa26df3f
Sergey1057 <sergei104@gmail.com>
2022-06-20 22:33:12 +0300

    first 1

--- app/src/main/java/com/example/myapplication/Equation.kt
+++ app/src/main/java/com/example/myapplication/Equation.kt
@@ -1,40 +1,34 @@
 package com.example.myapplication
 
-class Equation() {
-    private var a :Double = 0.0
-    private var b :Double = 0.0
-    private var c :Double = 0.0
+import java.lang.IllegalArgumentException
 
-    fun solve(a: Double, b: Double, c:Double): Array<Double> {
-        this.a = a
-        this.b = b
-        this.c = c
+class Equation {
 
-        val discriminant = getDiscriminant()
-        return if (discriminant > 0) {
+    fun solve(a: Double, b: Double, c:Double, e:Double = 1e-5 ): Array<Double> {
+        if (Math.abs(a) < e){
+            throw IllegalArgumentException("a < e")
+        }
+        if (a.isNaN()
+            || b.isNaN()
+            || c.isNaN()
+            || a.isInfinite()
+            || b.isInfinite()
+            || c.isInfinite()
+        ){
+            throw IllegalArgumentException("not number coefficient")
+        }
+
+        val discriminant = b * b - 4 * a * c
+         if (discriminant > e) {
                 val x1 = (-b - Math.sqrt(discriminant)) / (2 * a)
                 val x2 = (-b + Math.sqrt(discriminant)) / (2 * a)
-            arrayOf(x1,x2)
-            } else if (discriminant == 0.0 ) {
+             return arrayOf(x1,x2)
+            }
+        if (Math.abs(discriminant) < e) {
                 val x = -b / (2 * a)
-            arrayOf(x)
-            } else { emptyArray()
+            return arrayOf(x)
+            } else {
+            return emptyArray()
         }
-    }
-
-    private fun isQuadratic(): Boolean {
-        return a != 0.0
-    }
-
-    private fun isLinear(): Boolean {
-        return a == 0.0 && b != 0.0 && c != 0.0
-    }
-
-    private fun getDiscriminant(): Double {
-        return b * b - 4 * a * c
-    }
-
-    override fun toString(): String {
-        return "$a*x^2 + $b*x + $c = 0"
     }
 }
